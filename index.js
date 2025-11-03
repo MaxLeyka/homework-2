401acd28f349721f36cfedcab9acfb93512856b8
MaxLeyka <158818982+MaxLeyka@users.noreply.github.com>
2025-11-03 11:32:02 +0300

    feat: Сделана вторая функция - преобразование строки в объект

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const f = (a) =>{
+const fSum = (a) =>{
     return function (b) {
         return function (c) {
             return a+b+c
         }
     }
 }
-console.log(f(2)(3)(5))
+console.log(fSum(2)(3)(5))
+
+function strToObj(str) {
+    return str.split('.').reverse().reduce((acc, current) => ({
+        [current]: acc
+    }), {});
+}
+//console.log не выводит полностью весь объект
+console.dir(strToObj("one.two.tree.four.five"), { depth: 5 });
