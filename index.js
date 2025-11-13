f968f3671ed823696e0514df34f8956f87e07f02
Lazarenko Maxim <maxlaz435@gmail.com>
2025-11-13 13:29:21 +0300

    refactor: Доработана вторая функция: избавление от мутации начальных данных, добавлена обработка пустой строки, вывод сделан через JSON.stringify.

--- index.js
+++ index.js
@@ -17,9 +17,14 @@ console.log(Number(fSum(1)(2)(3)));
 console.log(Number(fSum(1)(2)(2)(1)));
 
 function strToObj(str) {
-    return str.split('.').reverse().reduce((acc, current) => ({
-        [current]: acc
-    }), {});
+    if (!str || typeof str !== 'string') return {};
+
+    return str
+        .split('.')
+        .reduceRight((acc, current) => ({
+            [current]: acc
+        }), {});
 }
-//console.log не выводит полностью весь объект
-console.dir(strToObj("one.two.tree.four.five"), { depth: 5 });
+
+console.log(JSON.stringify(strToObj("one.two.three.four.five"), null, 2));
+console.log(strToObj(""));
